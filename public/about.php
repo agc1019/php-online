67e6c4485a42b82e964a707b79bab3a4c80ebb12
agc1019 <160308854+agc1019@users.noreply.github.com>
2024-06-26 16:28:43 +0800

    Add files via upload

---
+++ public/about.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>About Insightify</title>
+    <link rel="icon" href="/logo/symbol_blue.png">
+    <meta name="title" content="Insightify">
+    <meta name="description" content="The website is an application simplify, translate, and paraphrase words using Google's Gemini.">
+    <meta name="keywords" content="Insightify, NLP, OCR, ">
+    <meta name="robots" content="noindex, nofollow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./css/about.css">
+    <link rel="stylesheet" href="./css/footer.css">
+    <link rel="stylesheet" href="./css/header_blue.css">
+
+    <!-- FOR ICONS -->
+    <script src="https://kit.fontawesome.com/ed08dfa832.js" crossorigin="anonymous"></script>
+
+    <script src="./js/dropdown_header.js" defer> </script>
+</head>
+
+<body>
+
+    <header>
+        <div class="logo_container">
+            <a href="">
+                <img class="logo" src="./logo/symbol_white.png" alt="logo"></img>
+            </a>
+        </div>
+                
+        <nav class="nav">
+
+            <div class="nav_links_container">
+
+                <ul class="nav_list">
+                    <li><a href="dashboard.php">Dashboard</a></li>
+                    <li><a href="collection.php">Collection</a></li>
+                    <li><a href="homepage.php">About</a></li>
+                    <li><a href="html_tools.php">Tools</a></li>
+                </ul>
+
+            </div>
+
+            <div class="dropdown_container"> 
+                <button class="dropdown_button">
+                    <img src="./img/profile.png" alt="Profile Icon">
+                    <i class="fa-solid fa-angle-down"></i>
+                </button>
+
+                <div class="dropdown_content">
+
+                    <div class="user_container">
+                        <img src="./img/profile.png" alt="Profile Icon">
+
+                        <div class="user_info_dropdown">
+                            <p style="font-weight: 700; font-size: 20px;"> @red.pogi</p>
+                            <p class="email_dropdown">email@gmail.com</p>
+                        </div>
+                        
+                    </div>
+
+                    <hr class="divider1">
+
+                    <a href="account_settings.php">Account Settings</a>
+                    <a href="about.php">About Insightify</a> 
+                    <a href="store_link_3">Get Insightify App</a>
+                    <a href="privacy_policy.php">Privacy Policy</a>
+                    <a href="contact.php">Contact Us</a>
+                    <a href="terms.php">Terms and Condition</a>
+
+                    <hr class="divider2">
+
+                    <a href="login-signup.php" style=" margin: 8px 0px;">Log Out</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+
+    <div class="hero">
+        <h1>About Us</h1>
+    </div>
+
+    <div class="title">
+        <!-- <h1>Who is</h1> -->
+        <img src="./logo/symbol+full_blue2.png" alt="logo">
+    </div>  
+
+    <div class="content">
+        <p>In today's digital age, information surrounds us, but accessing and comprehending it 
+            can be challenging. At Insightify, we believe that everyone deserves equal access to 
+            knowledge and the ability to understand complex information efficiently. This belief 
+            fueled our creation of Insightify, a comprehensive text analysis and manipulation toolkit 
+            designed to empower your learning experience.
+        </p>
+        <h1>Our Mission</h1>
+        <div class="mission">
+
+            <p>Image Scanner: Unlock the information held within images. Our OCR (Optical Character 
+                Recognition) technology accurately scans text from pictures, making it editable and searchable.
+            </p>
+            <p>Citation Generator: Never stress over formatting references again! Our generator helps you 
+                easily create accurate citations in various styles, ensuring academic integrity.
+            </p>
+            <p>Text Translator: Language barriers are a thing of the past. Our translator bridges communication 
+                gaps, enabling you to understand and be understood across different languages.
+            </p>
+
+        </div>
+
+            <p>
+                Our team, composed of passionate developers, educators, and language enthusiasts, is dedicated 
+                to creating innovative tools that contribute to a more informed and connected world. We are inspired 
+                by the potential of technology to empower individuals of all backgrounds and learning styles to 
+            engage with and utilize information effectively.
+        </p>
+        <p>
+            We believe that Insightify is more than just an application; it's a gateway to unlocking the potential 
+            within text and empowering you to navigate the world of information with confidence and clarity.
+        </p>
+
+        <h1>The Insightify Team</h1>
+
+    </div>
+    <div class="members">
+        <div class="card">
+            <div class="img">
+                <img src="./img/aris.jpg" alt="member">
+            </div>
+            <h1>Geraldine</h1>
+            <h2>Software Developer</h2>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="./img/dumalag.jpg" alt="member">
+            </div>
+            <h1>Iver</h1>
+            <h2>Software Developer</h2>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="./img/benson2.jpeg" alt="member">
+            </div>
+            <h1>Benson</h1>
+            <h2>Software Developer</h2>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="./img/madrunio.jpg" alt="member">
+            </div>
+            <h1>Samantha</h1>
+            <h2>Software Developer</h2>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="./img/ochavo.png" alt="member">
+            </div>
+            <h1>Redtide</h1>
+            <h2>Software Developer</h2>
+        </div>
+        
+    </div>
+
+
+    <footer>
+        <div class="footer_container">
+    
+            <div class="footer_columns">
+    
+                <div class="footer_column1">
+                    <img src="./logo/symbol+full_blue.png">
+                </div>
+    
+                <div class="footer_column2-4">
+    
+                    <div class="footer_header">
+                        <!-- COLUMN 1 HEADER -->
+                        <p>LET'S CONNECT</p>
+    
+                        <!-- COLUMN 2 HEADER -->
+                        <p>CONTACT US</p>
+    
+                        <!-- COLUMN 3 HEADER -->
+                        <p>DOWNLOAD INSIGHTIFY APP</p>
+                    </div>
+    
+    
+                    <div class="footer_text">
+                        <p> Whether you need assistance with app functionalities, encounter technical issues, or want to share your experience, we are here to help. Your satisfaction is our priority, and we are committed to providing you with the best user experience. </p>
+    
+                        <div class="footer_column2">
+                            <p><i class="fas fa-map-marker-alt icon-spacing"></i>Governor's Drive corner Aguinaldo Highway, Brgy. Sampaloc 1, Dasmariñas City, Cavite, Philippines</p>
+                            <p><i class="fas fa-envelope icon-spacing"></i><a href="mailto:insightify_customersupport1@gmail.com">insightify_customersupport1@gmail.com</a></p>
+                            <p><i class="fas fa-phone icon-spacing"></i>0921-253-1114</p>
+                        </div>
+                        
+                        <div class="footer_column3">
+                            <img src="./img/download_qr.png">
+                            <p>Download the Insightify app now for free!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    </footer>
+    
+
+</body>
+
+<style>
+    
+    
+</style>
+
+</html>
